4b6522ee396ef3832e73a657e5acbb30d542612e
Manjeet Singh <80199360+Manjeetdhimaan@users.noreply.github.com>
2023-03-25 12:15:21 +0530

    Add files via upload

---
+++ dev-env/dev-env.js
@@ -0,0 +1,6 @@
+module.exports.PORT = "3600";
+module.exports.API_URL = "/api/v1";
+module.exports.MONGO_URI = "mongodb+srv://manjeetuser:manjeet@employee-database.dxtzx.mongodb.net/ng-shop-test?retryWrites=true&w=majority";
+module.exports.JWT_SECRET = "jwtforngshopisverysecretcodeforme";
+module.exports.JWT_EXP =  "120m";
+module.exports.STRIPE_SECRET = "sk_test_51Mkl8USHSaj2D0AULPcWvx7kp04XueZJNoQrMhh6jEPjYUvAbIQUMUjrBEhZ6GKzdJdNWeUpJxunreO6EHjgE34u00gtfi6SYT";
