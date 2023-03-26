82c0daf539ab0eac520e92431eaf15d2144ac96a
Manjeet Singh <80199360+Manjeetdhimaan@users.noreply.github.com>
2023-03-26 16:04:30 +0530

    Update file.js

--- middlewares/file.js
+++ middlewares/file.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
         if (isValid) {
             error = null;
         }
-        cb(error, "tmp/uploads");
+        cb(error, "public/uploads");
         // cb(null, 'public/uploads')
     },
     filename: function (req, file, cb) {
