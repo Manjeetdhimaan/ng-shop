2c77d598042118c738d5eeacfea9e058471e7df1
Manjeet Singh <80199360+Manjeetdhimaan@users.noreply.github.com>
2023-03-26 08:40:06 +0530

    Add files via upload

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -10,8 +10,8 @@ router.get('/get-products-count', jwtHelper.verifyJwtToken, jwtHelper.isAdmin, c
 router.get('/get-featured-products/:count/:sort', ctrlProduct.getFeaturedProducts);
 router.get('/get-product/:id', ctrlProduct.getProduct);
 router.post('/post-product', jwtHelper.verifyJwtToken, jwtHelper.isAdmin, extractFile.single('image'), ctrlProduct.postProduct);
-router.put('/update-product/:id', jwtHelper.verifyJwtToken, jwtHelper.isAdmin, extractFile.single('image'), ctrlProduct.updateProduct);
-router.put('/update-product-gallery/:id', jwtHelper.verifyJwtToken, jwtHelper.isAdmin, extractFile.array('images', 20), ctrlProduct.updateProductGallery);
+router.put('update-product/:id', jwtHelper.verifyJwtToken, jwtHelper.isAdmin, extractFile.single('image'), ctrlProduct.updateProduct);
+router.put('update-product-gallery/:id', jwtHelper.verifyJwtToken, jwtHelper.isAdmin, extractFile.array('images', 20), ctrlProduct.updateProductGallery);
 router.delete('/delete-product/:id', jwtHelper.verifyJwtToken, jwtHelper.isAdmin, ctrlProduct.deleteProduct);
 
 module.exports = router;
